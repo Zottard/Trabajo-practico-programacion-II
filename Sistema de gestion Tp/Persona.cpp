c168c844835ee646a0d88d1007e109392350ff06
MateoEGomez <Gomezmateoezequiel@gmail.com>
2026-05-30 19:31:33 -0300

    Creacion de funciones Clase Persona

--- Sistema de gestion Tp/Persona.cpp
+++ Sistema de gestion Tp/Persona.cpp
@@ -1,6 +1,82 @@
+#include <iostream>
+#include <cstring>
 #include "Persona.h"
+using namespace std;
 
-Persona::Persona()
+Persona:: Persona(){
+
+}
+
+void Persona::setIdPersona(int idPersona)
 {
+    _idPersona=idPersona;
+}
 
+int Persona::getIdPersona()
+{
+    return _idPersona;
+}
+
+void Persona::setDniPersona(const char *dniPersona)
+{
+    strcpy(_dni,dniPersona);
+    _dni[9]='\0';
+}
+
+const char* Persona::getDniPersona()
+{
+    return _dni;
+}
+
+void Persona::setNombre(const char *nombre)
+{
+    strcpy(_nombre,nombre);
+    _nombre[49]='\0';
+}
+
+const char* Persona::getNombre()
+{
+    return _nombre;
+}
+
+void Persona::setApellido(const char *apellido)
+{
+    strcpy(_apellido,apellido);
+    _apellido[49]='\0';
+}
+
+const char* Persona::getApellido()
+{
+    return _apellido;
+}
+
+void Persona::setActivo(bool activo)
+{
+    _activo=activo;
+}
+
+bool Persona::getActivo()
+{
+    return _activo;
+}
+
+void Persona::cargarPersona()
+{
+    cout<<"Ingrese nombre: "<<endl;
+    cin>>_nombre;
+    cout <<"Ingrese apellido: "<<endl;
+    cin >> _apellido;
+    cout << "Ingrese DNI: "<<endl;
+    cin >> _dni;
+
+    _activo = true;
+}
+
+void Persona::mostrarPersona()
+{
+    if(_activo){
+        cout<<"NOMBRE: "<<_nombre<<endl;
+        cout<<"APELLIDO: "<<_apellido<<endl;
+        cout<<"DNI: "<<_dni<<endl;
+    }
 }
--- Sistema de gestion Tp/Persona.h
+++ Sistema de gestion Tp/Persona.h
@@ -2,8 +2,33 @@
 
 class Persona
 {
+    protected:
+    int _idPersona;
+    char _dni[10];
+    char _nombre[50];
+    char _apellido[50];
+    bool _activo;
+
     public:
         Persona();
+
+        void cargarPersona();
+        void mostrarPersona();
+
+        void setIdPersona(int);
+        int getIdPersona();
+
+        void setDniPersona(const char*);
+        const char* getDniPersona ();
+
+        void setNombre (const char*);
+        const char* getNombre();
+
+        void setApellido(const char*);
+        const char* getApellido ();
+
+        void setActivo(bool);
+        bool getActivo();
 
 
     private:
